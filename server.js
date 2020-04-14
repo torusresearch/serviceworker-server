7fe5e17d929b4e2828fae39c4454431fd4ccfea6
Leonard Tan <tetratorus@gmail.com>
2020-04-14 17:19:56 +0800

    Use cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express')
 var app = express()
 var fs = require('fs')
+var cors = require('cors')
 var https = require('https')
+app.use(cors())
 app.get('/serviceworker/sw.js', (req, res) => {
     res.write(fs.readFileSync('./public/sw.js'))
     res.status(200).end()
