a9c13b5b8ff57baae97743dff89a72168c9f067c
Leonard Tan <tetratorus@gmail.com>
2020-04-14 17:05:04 +0800

    Local server for torus-direct-web-sdk

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express()
+var fs = require('fs')
+var https = require('https')
+app.get('/serviceworker/sw.js', (req, res) => {
+    res.write(fs.readFileSync('./public/sw.js'))
+    res.status(200).end()
+})
+app.get('/serviceworker/redirect', (req, res) => {
+    res.write(fs.readFileSync('./public/redirect.html'))
+    res.status(200).end()
+})
+https.createServer({
+    cert: fs.readFileSync('./ssl/server.crt'),
+    key: fs.readFileSync('./ssl/server.key')
+}, app).listen(3000)
