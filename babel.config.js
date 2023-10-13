509d186eec5bd4f4108f9480d89745cd2f8e4f61
chaitanyapotti <chaitanya.potti@gmail.com>
2023-10-13 14:28:25 +0800

    v9 update

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   presets: ["@babel/env"],
-  plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/plugin-proposal-class-properties", "@babel/transform-runtime"],
+  plugins: ["@babel/transform-runtime"],
   sourceType: "unambiguous",
 };
